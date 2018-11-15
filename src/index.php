9bc5b39c30cde6e1bb61fbc764becb404e94f19f
Francisco Manzano <fmanzano@qdqmedia.com>
2018-11-15 09:42:37 +0100

    Basic structure

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Se supone q esto es php</h1>
+    <?php echo '<p>Hola Mundo</p>'; ?>
+    <p>Mas cosas de php</p>
+</body>
+</html>
