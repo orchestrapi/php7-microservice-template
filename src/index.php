b881a1d84e850c26876363c5b6d65b05b4a1d41f
Francisco Manzano Magaña <keoh77@gmail.com>
2018-12-14 21:35:01 +0100

    Instance variable on index.php and Template message

--- src/index.php
+++ src/index.php
@@ -4,12 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>PHP7 Template Project | Index</title>
 </head>
 <body>
-    <h1>Se supone q esto es php</h1>
-    <?php echo '<p>Hola Mundo</p>'; ?>
-    <p>Mas cosas de php</p>
+    <h1>PHP7 Template Project</h1>
     <p>Version: <?php echo getenv('VERSION');  ?></p>
+    <p>Instance: <?php echo getenv('INSTANCE');  ?></p>
 </body>
 </html>
